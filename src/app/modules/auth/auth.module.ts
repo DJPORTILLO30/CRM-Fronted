01e5eec18dabad79325bd2dc83ecfa2a9bb9e4db
Julio Fuentes <juliofuentes12@hotmail.com>
2022-10-28 20:35:50 -0600

    Solucion problemas de merge

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,12 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
-<<<<<<< HEAD
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
-=======
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> 7b3ab683cedd515730bea406f4554d392b1205ea
 
 
 @NgModule({
