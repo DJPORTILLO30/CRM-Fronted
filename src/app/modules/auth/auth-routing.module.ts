d16ffd3b8994c0ba886f31cf1086bcfc9a9c4c7d
Yessenia Mateo <yesselimateo@gmail.com>
2022-10-28 20:38:24 -0600

    correcciones

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
-const routes: Routes = [
-  {
-    path:'login-admin',
-    component:AdminPageComponent
-  },
-=======
+  
 import { SessionGuard } from '@core/guards/session.guard';
 import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 import { AuthPageComponent} from './pages/auth-page/auth-page.component'
@@ -24,12 +19,15 @@ const routes: Routes = [
     component:RegisterPageComponent,
   },
   {
+    path:'login-admin',
+    component:AdminPageComponent
+  },
+  {
     path: '',
     component: HomePageComponent,
     loadChildren:()=> import(`../home/home.module`).then(m=> m.HomeModule),
     canActivate:[SessionGuard]
   }
->>>>>>> 7b3ab683cedd515730bea406f4554d392b1205ea
 ];
 
 @NgModule({
