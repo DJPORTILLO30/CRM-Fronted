6376d0369e5a1a2cd17ddb120cc90c9720face66
Hazzel3 <moraleshazel5@gmail.com>
2022-11-02 16:09:34 +0000

    se quito el interceptor del bussines del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { InterceptorBussinesService } from '@core/interceptors/bussines/interceptor-bussines.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,11 +16,7 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: InterceptorBussinesService,
-    multi: true
-  }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
