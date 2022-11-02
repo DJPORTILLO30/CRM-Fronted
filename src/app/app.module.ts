a24a12b9daae2086c396fc77b9a37da589d286d1
Hazzel3 <moraleshazel5@gmail.com>
2022-11-01 21:55:41 -0600

    Instalar angular material en el proyecto, arreglar la vista del home (css) y se agregaron las rutas del backend en los archivos de varibles de entorno, y se agrego el modelo y el interceptor de bussnies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { InterceptorBussinesService } from '@core/interceptors/bussines/interceptor-bussines.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: InterceptorBussinesService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/models/bussines/bussines.ts
@@ -0,0 +1,14 @@
+export class Bussines {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public contactName: string,
+    public amount: number,
+    public income: number,
+    public closingDate: Date,
+    public campaingSource: string,
+    public description: string
+  ) { }
+
+}
