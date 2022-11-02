c907723d5d77e427fcde93202276b82ee4c4afe1
Danemora23 <112773098+DarwinDane@users.noreply.github.com>
2022-11-01 22:24:35 -0600

    Se creo el dialogo del formulario de creacion, editar y eliminar con busisnes

--- src/app/modules/bussnies/bussnies.module.ts
+++ src/app/modules/bussnies/bussnies.module.ts
@@ -1,17 +1,56 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
 
 import { BussniesRoutingModule } from './bussnies-routing.module';
 import { BussniesPageComponent } from './pages/bussnies-page/bussnies-page.component';
-
+import { DialogFormBussinesComponent } from './utils/dialog-form-bussines/dialog-form-bussines.component';
 
 @NgModule({
   declarations: [
-    BussniesPageComponent
+    BussniesPageComponent,
+    DialogFormBussinesComponent
   ],
   imports: [
     CommonModule,
-    BussniesRoutingModule
+    BussniesRoutingModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class BussniesModule { }
