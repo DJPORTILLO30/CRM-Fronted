0b6c24ce77ac350e2bce75d136cb4eab6d8cd637
PDHElite <60772764+PDHElite@users.noreply.github.com>
2022-11-03 02:04:38 -0600

    Se finalizo la creacion de la tabla de productos y sus acciones

--- src/app/modules/products/dialog/dialog.component.ts
+++ src/app/modules/products/dialog/dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '@shared/services/servicesproducts/api.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-dialog',
@@ -9,8 +12,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class DialogComponent implements OnInit {
 
   productsForm!: FormGroup;
+  actionBtn: String = 'Save';
 
-  constructor(private formbuilder : FormBuilder ) { }
+  constructor(private formbuilder : FormBuilder, 
+    private api: ApiService, 
+    @Inject(MAT_DIALOG_DATA) public editData: any,
+    private dialogRef : MatDialogRef<DialogComponent> ) { }
 
   ngOnInit(): void {
 
@@ -20,11 +27,56 @@ export class DialogComponent implements OnInit {
       Category : ['', Validators.required],
       Price : ['', Validators.required],
       Feature : ['', Validators.required]
-    })
+    });
+
+    if(this.editData){
+      this.actionBtn = "Update";
+      this.productsForm.controls['name'].setValue(this.editData.name);
+      this.productsForm.controls['description'].setValue(this.editData.description);
+      this.productsForm.controls['Category'].setValue(this.editData.Category);
+      this.productsForm.controls['Price'].setValue(this.editData.Price);
+      this.productsForm.controls['Feature'].setValue(this.editData.Feature);
+    }
+
   }
 
   insertProducts(){
-    console.log(this.productsForm.value);
+    if(!this.editData){
+      if(this.productsForm.valid){
+        this.api.insertProducts(this.productsForm.value)
+        .subscribe({
+          next: (res) =>{
+            alert("Producto agregado exitosamente"),
+            this.productsForm.reset();
+            this.dialogRef.close('save');
+          },
+          error: () =>{
+            alert("Error agregando el producto")
+          }
+        })
+      }
+    }
+    
+    
+    else{
+      this.updateProducts()
+    }
   }
+
+  updateProducts(){
+    this.api.UpdateProducts(this.productsForm.value, this.editData.id)
+    .subscribe({
+      next: (res) =>{
+        alert("Producto Modificado Exitosamente");
+        this.productsForm.reset();
+        this.dialogRef.close('update');
+      },
+      error: () =>{
+        alert("Error al Modificar el Producto");
+      }
+    })
+  }
+
+
 
 }
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -16,6 +16,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 
+
 import { DialogComponent } from './dialog/dialog.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
