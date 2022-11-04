647bd89d5c6fc8558ffa466e4ec46aff08b251df
PDHElite <60772764+PDHElite@users.noreply.github.com>
2022-11-04 13:34:49 -0600

    Se arreglo la conexion de la api get de productos.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+/*private readonly URL = environment*/
 
 export const environment = {
-  production: false
+  production: false,
+  api:'https://crm-umg2022.herokuapp.com/api'
 };
 
 /*
